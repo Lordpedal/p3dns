3839c7af029c7320a438893bec115beeddd624cd
Lordpedal <39012148+Lordpedal@users.noreply.github.com>
2020-12-04 18:07:21 +0100

    Create listads.sh

---
+++ listads.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# Actualizador Lista Anti-Publicidad
+#
+while true
+do
+    if ping -c 1 -W 5 google.com 1>/dev/null 2>&1
+    then
+        echo "Actualizando lista ADS..."
+        /usr/bin/curl --silent --output ~/docker/piholeproxy/dnscrypt-proxy/blacklist.txt \
+          https://download.dnscrypt.info/blacklists/domains/mybase.txt >/dev/null 2>&1
+        break
+    else
+        echo "Conexion no establecida..."
+    fi
+    sleep 1
+done
