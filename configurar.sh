d609d264e758c04cd39a92e43b697d33e0094d5d
Lordpedal <39012148+Lordpedal@users.noreply.github.com>
2020-12-05 22:56:10 +0100

    Update configurar.sh

--- configurar.sh
+++ configurar.sh
@@ -6,7 +6,7 @@ echo -e "\e[1;33mConfigurando Docker: Pi-hole\e[0m"
 cp docker-compose.p3 docker-compose.yml
 read -p "Puerto web para gestion Pi-hole (recomendado 83): " puerto
 sed -i "s/83/$puerto/g" "docker-compose.yml"
-read -p "Contraseña web para gestion Pi-hole: " password
+read -p "Contraseña web para gestion Pi-hole (por defecto lordpedal): " password
 sed -i "s/lordpedal/$password/g" "docker-compose.yml"
 chmod +x listads.sh
 echo -e "\e[0;32mP3DNS: Configurado\e[0m"
