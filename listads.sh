37c664781cb33840380388a0fe238fa4e5ba3b45
Lordpedal <39012148+Lordpedal@users.noreply.github.com>
2020-12-06 13:26:46 +0100

    Update listads.sh

--- listads.sh
+++ listads.sh
@@ -7,7 +7,7 @@ do
     if ping -c 1 -W 5 google.com 1>/dev/null 2>&1
     then
         echo "Actualizando lista ADS..."
-        /usr/bin/curl --silent --output ~/docker/piholeproxy/dnscrypt-proxy/blacklist.txt \
+        /usr/bin/curl --silent --output ~/docker/p3dns/dnscrypt-proxy/blacklist.txt \
           https://download.dnscrypt.info/blacklists/domains/mybase.txt >/dev/null 2>&1
         break
     else
